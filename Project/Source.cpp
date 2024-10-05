b3758d07ce81c55c73aded59b611af0859dea5d4
Cseh Kristóf Dávid <csehkristofdavid3576@gmail.com>
2024-10-05 17:37:38 +0200

    setup coordinate system

--- Project/Source.cpp
+++ Project/Source.cpp
@@ -1,87 +1,81 @@
-///////////////////////////////////
-// square.cpp
-//
-// OpenGL program to draw a square.
-// 
-// Sumanta Guha.
-///////////////////////////////////
-
 #include <GL/glew.h>
 #include <GL/freeglut.h> 
 
 // Drawing routine.
 void drawScene(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-	glColor3f(0.0, 0.0, 0.0);
+    glBegin(GL_LINES);
 
-	// Draw a polygon with specified vertices.
-	glBegin(GL_POLYGON);
-	glVertex3f(20.0, 20.0, 0.0);
-	glVertex3f(80.0, 20.0, 0.0);
-	glVertex3f(80.0, 80.0, 0.0);
-	glVertex3f(20.0, 80.0, 0.0);
-	glEnd();
+    // X tengely (piros)
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(3.0, 0.0, 0.0);
 
-	glFlush();
+    // Y tengely (zöld)
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(0.0, 3.0, 0.0);
+
+    // Z tengely (kék)
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(0.0, 0.0, 3.0);
+
+    glEnd();
+
+    glFlush();
 }
 
 // Initialization routine.
 void setup(void)
 {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
+    glClearColor(1.0, 1.0, 1.0, 0.0);
 }
 
 // OpenGL window reshape routine.
 void resize(int w, int h)
 {
-	glViewport(0, 0, w, h);
+    glViewport(0, 0, w, h);
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0.0, 100.0, 0.0, 100.0, -1.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0, (float)w / (float)h, 1.0, 100.0);
 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(3.0, 3.0, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
-// Keyboard input processing routine.
+
 void keyInput(unsigned char key, int x, int y)
 {
-	switch (key)
-	{
-	case 27:
-		exit(0);
-		break;
-	default:
-		break;
-	}
 }
 
 // Main routine.
 int main(int argc, char** argv)
 {
-	glutInit(&argc, argv);
+    glutInit(&argc, argv);
 
-	glutInitContextVersion(4, 3);
-	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+    glutInitContextVersion(4, 3);
+    glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
 
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
 
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(100, 100);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
 
-	glutCreateWindow("square.cpp");
+    glutCreateWindow("3D Koordináta Rendszer");
 
-	glutDisplayFunc(drawScene);
-	glutReshapeFunc(resize);
-	glutKeyboardFunc(keyInput);
+    glutDisplayFunc(drawScene);
+    glutReshapeFunc(resize);
+    glutKeyboardFunc(keyInput);
 
-	glewExperimental = GL_TRUE;
-	glewInit();
+    glewExperimental = GL_TRUE;
+    glewInit();
 
-	setup();
+    setup();
 
-	glutMainLoop();
+    glutMainLoop();
 }
