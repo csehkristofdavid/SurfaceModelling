82b03972b26fda0f44b11714229a252f3a4d36ab
Novák Dániel <novakdaniel0805@gmail.com>
2024-10-13 21:33:51 +0200

    Hard coded Surface

--- Project/Source.cpp
+++ Project/Source.cpp
@@ -4,57 +4,104 @@
 float rotationX = 0.0;
 float rotationY = 0.0;
 
+// 4x4-es kontrollpontok vízszintes felülethez (X-Z síkban)
+GLfloat ctrlPoints[4][4][3] = {
+    { {-1.5, 0.0, -1.5}, {-0.5, 0.0, -1.5}, {0.5, 0.0, -1.5}, {1.5, 0.0, -1.5} },
+    { {-1.5, 0.0, -0.5}, {-0.5, 0.0, -0.5}, {0.5, 0.0, -0.5}, {1.5, 0.0, -0.5} },
+    { {-1.5, 0.0, 0.5}, {-0.5, 0.0, 0.5}, {0.5, 0.0, 0.5}, {1.5, 0.0, 0.5} },
+    { {-1.5, 0.0, 1.5}, {-0.5, 0.0, 1.5}, {0.5, 0.0, 1.5}, {1.5, 0.0, 1.5} }
+};
+
+void drawControlPoints()
+{
+    glPointSize(5.0);
+    glColor3f(0.0, 0.0, 0.0);  // Fekete színű pontok
+    glBegin(GL_POINTS);
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            glVertex3fv(ctrlPoints[i][j]);
+        }
+    }
+    glEnd();
+}
+
+void drawControlLines()
+{
+    glColor3f(0.0, 0.0, 0.0);  // Fekete színű vonalak
+
+    // Sorok összekötése
+    for (int i = 0; i < 4; ++i)
+    {
+        glBegin(GL_LINE_STRIP);
+        for (int j = 0; j < 4; ++j)
+        {
+            glVertex3fv(ctrlPoints[i][j]);
+        }
+        glEnd();
+    }
+
+    // Oszlopok összekötése
+    for (int j = 0; j < 4; ++j)
+    {
+        glBegin(GL_LINE_STRIP);
+        for (int i = 0; i < 4; ++i)
+        {
+            glVertex3fv(ctrlPoints[i][j]);
+        }
+        glEnd();
+    }
+}
+
 void drawScene(void)
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // model view
+    // Model view
     glLoadIdentity();
 
-    // cam pos
+    // Cam pos
     gluLookAt(5.0, 5.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-    // rotate
+    // Rotate
     glRotatef(rotationX, 1.0, 0.0, 0.0);
     glRotatef(rotationY, 0.0, 1.0, 0.0);
 
-    // draw
+    // Draw axes
     glBegin(GL_LINES);
-
-    // X
-    glColor3f(1.0, 0.0, 0.0);
+    glColor3f(1.0, 0.0, 0.0); // X tengely
     glVertex3f(0.0, 0.0, 0.0);
     glVertex3f(3.0, 0.0, 0.0);
-
-    // Y
-    glColor3f(0.0, 1.0, 0.0);
+    glColor3f(0.0, 1.0, 0.0); // Y tengely
     glVertex3f(0.0, 0.0, 0.0);
     glVertex3f(0.0, 3.0, 0.0);
-
-    // Z
-    glColor3f(0.0, 0.0, 1.0);
+    glColor3f(0.0, 0.0, 1.0); // Z tengely
     glVertex3f(0.0, 0.0, 0.0);
     glVertex3f(0.0, 0.0, 3.0);
-
     glEnd();
+
+    // Kontrollpontok és vonalak rajzolása
+    drawControlPoints();
+    drawControlLines();
 
     glFlush();
 }
 
-// init
+// Inicializációs rutin
 void setup(void)
 {
     glClearColor(1.0, 1.0, 1.0, 0.0);
+    glEnable(GL_DEPTH_TEST);
+    glShadeModel(GL_FLAT);
 }
 
 void resize(int w, int h)
 {
     glViewport(0, 0, w, h);
-
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(45.0, (float)w / (float)h, 1.0, 100.0);
-
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -63,39 +110,35 @@ void specialInput(int key, int x, int y)
     float angleStep = 5.0;
     switch (key)
     {
-        case GLUT_KEY_RIGHT:
-            rotationY += angleStep;
-            break;
-        case GLUT_KEY_LEFT:
-            rotationY -= angleStep;
-            break;
-        case GLUT_KEY_UP:
-            rotationX -= angleStep;
-            break;
-        case GLUT_KEY_DOWN:
-            rotationX += angleStep;
-            break;
+    case GLUT_KEY_RIGHT:
+        rotationY += angleStep;
+        break;
+    case GLUT_KEY_LEFT:
+        rotationY -= angleStep;
+        break;
+    case GLUT_KEY_UP:
+        rotationX -= angleStep;
+        break;
+    case GLUT_KEY_DOWN:
+        rotationX += angleStep;
+        break;
     }
-    glutPostRedisplay(); // display again
+    glutPostRedisplay();
 }
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-
     glutInitContextVersion(4, 3);
     glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
-
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
     glutInitWindowSize(800, 600);
     glutInitWindowPosition(100, 100);
-
     glutCreateWindow("Surface Modelling");
 
     glutDisplayFunc(drawScene);
     glutReshapeFunc(resize);
-    glutSpecialFunc(specialInput); // Ny�lbillenty�k kezel�s�hez.
+    glutSpecialFunc(specialInput);
 
     glewExperimental = GL_TRUE;
     glewInit();
