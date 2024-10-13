c3bc37eafce9a14fb7081a32a66f2acbb3e559c7
Cseh Kristóf Dávid <csehkristofdavid3576@gmail.com>
2024-10-13 22:18:05 +0200

    being able to select points

--- Project/Source.cpp
+++ Project/Source.cpp
@@ -1,8 +1,10 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
+#include <cmath>
 
 float rotationX = 0.0;
 float rotationY = 0.0;
+int selectedPoint[2] = { -1, -1 }; // A kiválasztott pont indexei (-1, -1, ha nincs kiválasztva)
 
 // 4x4-es kontrollpontok vízszintes felülethez (X-Z síkban)
 GLfloat ctrlPoints[4][4][3] = {
@@ -12,19 +14,38 @@ GLfloat ctrlPoints[4][4][3] = {
     { {-1.5, 0.0, 1.5}, {-0.5, 0.0, 1.5}, {0.5, 0.0, 1.5}, {1.5, 0.0, 1.5} }
 };
 
+void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius)
+{
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < 100; i++)
+    {
+        float angle = 2.0f * 3.1415926f * float(i) / float(100);
+        float dx = radius * cosf(angle);
+        float dz = radius * sinf(angle);
+        glVertex3f(x + dx, y, z + dz);
+    }
+    glEnd();
+}
+
 void drawControlPoints()
 {
-    glPointSize(5.0);
-    glColor3f(0.0, 0.0, 0.0);  // Fekete színű pontok
-    glBegin(GL_POINTS);
+    glColor3f(0.0, 0.0, 0.0); // Fekete színű körök
     for (int i = 0; i < 4; ++i)
     {
         for (int j = 0; j < 4; ++j)
         {
-            glVertex3fv(ctrlPoints[i][j]);
+            drawCircle(ctrlPoints[i][j][0], ctrlPoints[i][j][1], ctrlPoints[i][j][2], 0.05);
         }
     }
-    glEnd();
+
+    // Kiemeljük a kiválasztott pontot, ha van.
+    if (selectedPoint[0] != -1 && selectedPoint[1] != -1)
+    {
+        glColor3f(1.0, 0.0, 0.0); // Piros színű kör a kiválasztott pont körül
+        drawCircle(ctrlPoints[selectedPoint[0]][selectedPoint[1]][0],
+            ctrlPoints[selectedPoint[0]][selectedPoint[1]][1],
+            ctrlPoints[selectedPoint[0]][selectedPoint[1]][2], 0.1);
+    }
 }
 
 void drawControlLines()
@@ -88,6 +109,54 @@ void drawScene(void)
     glFlush();
 }
 
+// Egérkattintás kezelése
+void mouseFunc(int button, int state, int x, int y)
+{
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        // Képernyő koordinátákból OpenGL koordinátákra való átalakítás
+        GLint viewport[4];
+        GLdouble modelview[16];
+        GLdouble projection[16];
+        GLfloat winX, winY, winZ;
+        GLdouble posX, posY, posZ;
+
+        glGetIntegerv(GL_VIEWPORT, viewport);
+        glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+        glGetDoublev(GL_PROJECTION_MATRIX, projection);
+
+        winX = (float)x;
+        winY = (float)viewport[3] - (float)y;
+        glReadPixels(x, (int)winY, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ);
+        gluUnProject(winX, winY, winZ, modelview, projection, viewport, &posX, &posY, &posZ);
+
+        // Kiválasztott pont keresése
+        for (int i = 0; i < 4; ++i)
+        {
+            for (int j = 0; j < 4; ++j)
+            {
+                float dx = posX - ctrlPoints[i][j][0];
+                float dy = posY - ctrlPoints[i][j][1];
+                float dz = posZ - ctrlPoints[i][j][2];
+                float distance = sqrt(dx * dx + dy * dy + dz * dz);
+
+                if (distance < 0.1) // Ha elég közel van a pont
+                {
+                    selectedPoint[0] = i;
+                    selectedPoint[1] = j;
+                    glutPostRedisplay();
+                    return;
+                }
+            }
+        }
+
+        // Ha nem kattintottunk pont közelébe
+        selectedPoint[0] = -1;
+        selectedPoint[1] = -1;
+        glutPostRedisplay();
+    }
+}
+
 // Inicializációs rutin
 void setup(void)
 {
@@ -139,6 +208,7 @@ int main(int argc, char** argv)
     glutDisplayFunc(drawScene);
     glutReshapeFunc(resize);
     glutSpecialFunc(specialInput);
+    glutMouseFunc(mouseFunc);
 
     glewExperimental = GL_TRUE;
     glewInit();
